435fc2be56cd446cebab0bf4aa003c49ea77601d
Jose Garmilla <garmilla@princeton.edu>
2014-12-03 23:19:19 -0500

    Add option to _updateVr method in order to choose the way the temperature is updated at each time step.

--- scripts/convection.py
+++ scripts/convection.py
@@ -76,8 +76,7 @@ class circumbinary(object):
         self.Omega = np.sqrt(G*M/r**3)
         self.TvThin = np.power(9.0/4*alpha*k/sigma/mu/kappa0*self.Omega, 1.0/(3.0+beta))
         self.Ti = np.power(np.square(eta/7*L/4/np.pi/sigma)*k/mu/G/M*r**(-3), 1.0/7)
-        TvThick = np.power(27.0/64*kappa0*alpha*k/sigma/mu*self.Omega*self.Sigma**2, 1.0/(3.0-beta))
-        self.T.setValue(np.power(self.TvThin**4 + TvThick**4 + self.Ti**4, 1.0/4))
+        self.T.setValue(self._interpT())
         self.T.updateOld()
         # Create a binary operator variable for h/r
         self.hr = CellVariable(name='Ratio of thickness to radius, h/r',
@@ -86,15 +85,9 @@ class circumbinary(object):
 
     def _genVr(self):
         """Generate the face variable that stores the velocity values"""
-        r = self.r*r0 #In physical units (cgs)
-        self.Omega = np.sqrt(G*M/r**3)
-        self.TvThin = np.power(9.0/4*alpha*k/sigma/mu/kappa0*self.Omega, 1.0/(3.0+beta))
-        self.Ti = np.power(np.square(eta/7*L/4/np.pi/sigma)*k/mu/G/M*r**(-3), 1.0/7)
-        TvThick = np.power(27.0/64*kappa0*alpha*k/sigma/mu*self.Omega*self.Sigma**2, 1.0/(3.0-beta))
-        T = np.power(self.TvThin**4 + TvThick**4 + self.Ti**4, 1.0/4)
-        r /= r0
+        r = self.r #In dimensionless units (cgs)
         # viscosity at cell centers in cgs
-        nu = alpha*k*T/mu/self.Omega
+        nu = alpha*k*self.T/mu/self.Omega
         self.visc = CellVariable(name='Viscous Term', mesh=self.mesh)
         self.visc.setValue(r**0.5*nu*self.Sigma)
         # Create a face variable to store the values of the radial velocity
@@ -111,15 +104,27 @@ class circumbinary(object):
         # The current scheme is an implicit-upwind
         self.eq = TransientTerm() == - UpwindConvectionTerm(coeff=self.vr)
 
-    def _updateVr(self):
+    def _updateVr(self, mode='interp'):
         """
         Update the value of the velocity. This function assumes the value of
         Sigma has been updated by the solve or sweep method.
+
+        Keywords
+        mode: If equal to `interp`, use the simple interpolation scheme to update
+              the temperature. If equal to `exact`, attempt an exact solutuion of
+              the temperature equation using a nonlinear solver. If equal to
+              `iterate`, use an iteration scheme on the temperature equation to
+              approximate the solution.
         """
-        r = self.mesh.cellCenters.value[0]
-        TvThick = np.power(27.0/64*kappa0*alpha*k/sigma/mu*self.Omega*self.Sigma**2, 1.0/(3.0-beta))
-        T = np.power(self.TvThin**4 + TvThick**4 + self.Ti**4, 1.0/4)
-        nu = alpha*k*T/mu/self.Omega
+        r = self.r
+        if mode == 'interp':
+            self.T.setValue(self._interpT())
+            self.T.updateOld()
+        elif mode == 'exact':
+            self._exactT()
+        elif mode == 'iterate':
+            self._iterT()
+        nu = alpha*k*self.T/mu/self.Omega
         self.visc.setValue(r**0.5*nu*self.Sigma)
         rF = self.mesh.faceCenters.value # radii at the cell faces
         self.vr.setValue(-3/r0**2/rF**(0.5)/(self.Sigma.faceValue + self.delta)*self.visc.faceGrad())
@@ -155,7 +160,6 @@ class circumbinary(object):
         Firr = 0.5*L/4/np.pi/r*np.maximum(self.hr.grad.value[0], 0.0)
         self.T.setValue(np.power((3*tau/4 + 1.0/(tau+delta))*Fnu + Firr, 0.25)/sigma)
 
-
     def iterT(self):
         """
         Iterate over temperature to get a more reliable solution
@@ -163,7 +167,7 @@ class circumbinary(object):
         for i in range(self.titer):
             self._iterT()
 
-    def exactT(self, init='old'):
+    def _exactT(self, init='old'):
         """
         Get the exact solution for T
         keywords:
