7ccec54c3158a06f10e024036f45ff4d0082902e
Jose Garmilla <garmilla@astro.princeton.edu>
2015-02-09 16:58:38 -0500

    Bug fixes to thermopy.

--- scripts/thermopy.py
+++ scripts/thermopy.py
@@ -42,19 +42,25 @@ def func2(T, r, Sigma, q, f):
            + fv(r,T,Sigma)) - sigma*Tirr(r)**4
 
 def Tfin(Tcheck, r, Sigma, q, f):
-    if Tcheck > 202.677:
+    if Tcheck >= 202.677:
         return Tcheck
     else:
         try:
             T1 =  brentq(func1,166.8099,202.677001,args=(r,Sigma,q,f), maxiter=200)
         except ValueError:
-            return brentq(func2,1,204,args=(r,Sigma,q,f), maxiter=200)
-        if 166.81 < T1 < 202.677:
+            #print "args1=", r, Sigma, q, f
+            #Ts = np.linspace(166.8099, 202.677001, num=1000)
+            #ys = func1(Ts, r, Sigma, q, f)
+            #plt.semilogx(Ts, np.log10(ys+1.0))
+            #plt.semilogx(Ts, np.log10(-ys+1.0))
+            #plt.show()
+            return brentq(func2,1.0e-3,166.81001,args=(r,Sigma,q,f), maxiter=200)
+        if 166.81 <= T1 <= 202.677:
             return T1
         else:
-            return brentq(func2,0.01,166.81001,args=(r,Sigma,q,f), maxiter=200)
+            return brentq(func2,1.0e-3,166.81001,args=(r,Sigma,q,f), maxiter=200)
 
-def buildTempTable(rGrid, q=1.0, f=0.001, Tmin=1.0, Tmax=500000, Sigmin=1.0e-5, Sigmax=1500, Sigres=2000, **kargs):
+def buildTempTable(rGrid, q=1.0, f=0.001, Tmin=202.6769, Tmax=1.0e6, Sigmin=1.0e-5, Sigmax=1.0e4, Sigres=2000, **kargs):
     """
     Return a table of precomputed temperatures as a function of radius and surface density.
 
@@ -76,7 +82,20 @@ def buildTempTable(rGrid, q=1.0, f=0.001, Tmin=1.0, Tmax=500000, Sigmin=1.0e-5, 
     temp = np.zeros((len(rGrid), Sigres)) #create m x n array for temp
     for i, r in enumerate(rGrid):
         for j, Sigma in enumerate(SigmaGrid):
-            Tcheck = brentq(func, Tmin, Tmax, args=(r,Sigma,q,f), maxiter=200)
-            temp[i,j] = Tfin(Tcheck, r, Sigma, q, f)
+            try:
+                Tcheck = brentq(func, Tmin, Tmax, args=(r,Sigma,q,f), maxiter=200)
+                temp[i,j] = Tfin(Tcheck, r, Sigma, q, f)
+            except ValueError:
+                try:
+                    temp[i,j] = Tfin(1.0, r, Sigma, q, f)
+                except ValueError:
+                    print "args2=", r, Sigma, q, f
+                    Ts = np.linspace(1.0e-3, 166.813, num=1000)
+                    ys = func2(Ts, r, Sigma, q, f)
+                    #plt.semilogx(Ts, np.log10(ys+1.0))
+                    #plt.semilogx(Ts, np.log10(-ys+1.0))
+                    plt.semilogx(Ts, ys)
+                    plt.show()
+                    raise
     # Return values in logspace for interpolation
     return np.log10(rGrid), np.log10(SigmaGrid), np.log10(temp)
