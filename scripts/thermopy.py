0600d267f1b7c5f5fd98453b9e7cd49f8b2f84e6
dvartany <vartanyandavid@yahoo.com>
2015-03-30 16:20:03 -0400

    bug fix

--- scripts/thermopy.py
+++ scripts/thermopy.py
@@ -30,7 +30,7 @@ def fv(r, T, Sigma):
     return 1.125 * Omega(r)*alpha*k*T/mu * Sigma
 
 def Tirr(r, q):
-    if q = 0:
+    if q == 0:
         return (((eta/7.0)*0.5*L/(4*np.pi*sigma))**2* k/(G*M*mu))**(1.0/7.0)*r**(-3.0/7.0)
     else:
         return (((eta/7.0)*2*L/(4*np.pi*sigma))**2* k/(G*M*mu))**(1.0/7.0)*r**(-3.0/7.0)
