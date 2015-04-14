ef5c3b3c59c55448123b23fbe651bcb2065a73e6
dvartany <vartanyandavid@yahoo.com>
2015-04-14 01:47:05 -0400

    bug fix

--- scripts/utils.py
+++ scripts/utils.py
@@ -564,6 +564,8 @@ def getSED(circ, extrap=False, power=1.0/0.95, Teff=None, Tsh=None, tau=None, nL
             Teff = (2.0/3/np.pi)**0.25*(Rs/r)**0.75 * Ts
         if Tsh is None:
             Tsh = np.power(L/16/np.pi/sigma/0.1/(r)**2, 0.25)
+            for i in range(0,np.where(Tsh < 1500)[0][0]):
+                Tsh[i] = 0
         Firr = sigma*thm.Tirr(r, circ.q)**4
         for i in range(len(nu)):
             x = r
@@ -582,8 +584,6 @@ def getSED(circ, extrap=False, power=1.0/0.95, Teff=None, Tsh=None, tau=None, nL
             Teff = getTeff(circ, tau=tau)
         if Tsh is None:
             Tsh = np.power(L/16/np.pi/sigma/0.1/(r)**2, 0.25)
-            for i in range(0,np.where(Tsh < 1500)[0][0]):
-                Tsh[i] = 0
         Firr = sigma*thm.Tirr(r, circ.q)**4
         for i in range(len(nu)):
             x = r
