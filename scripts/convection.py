2620b8e7a5e6ff8101ceafc4c328519c6cb2e650
Jose Garmilla <garmilla@princeton.edu>
2015-09-17 16:58:35 -0400

    Change from cell to face variable for source term.

--- scripts/convection.py
+++ scripts/convection.py
@@ -272,9 +272,9 @@ class Circumbinary(object):
         if self.q > 0.0:
             if torqueAsSource:
                 self.vr = self.vrVisc
-                r32 = np.power(self.r, 1.5)
+                r32 = np.power(self.rF, 1.5)
                 self.eq = TransientTerm(var=self.Sigma) == - ExplicitUpwindConvectionTerm(coeff=self.vr, var=self.Sigma)\
-                                                           - (self.LambdaCell*self.Sigma.old*r32).getDivergence()
+                                                           - (self.Lambda*self.Sigma.old.faceValue*r32).divergence
             else:
                 self.vr = self.vrVisc + self.vrTid
                 self.eq = TransientTerm(var=self.Sigma) == - ExplicitUpwindConvectionTerm(coeff=self.vr, var=self.Sigma)
